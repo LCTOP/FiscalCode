079516a0021c51c7d7476ae016e39efa94e53a72
Lorenzo Calamandrei <lorenzo.calamandrei@triboo.it>
2018-02-09 09:38:27 +0100

    Master Init

---
+++ src/FiscalCode/Calculator.php
@@ -0,0 +1,17 @@
+<?php
+namespace FiscalCode;
+
+/**
+ * Class Calculator
+ *
+ * Class for calculate fiscal code by user data
+ * @author Michael Zangirolami <michael.zangirolami@triboo.it>
+ * @author Lorenzo Calamandrei <lorenzo.calamandrei@triboo.it>
+ * @version 0.1.0
+ * @package FiscalCode
+ */
+class Calculator
+{
+}
+
+
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CalculatorTest
+ */
+final class CalculatorTest extends TestCase
+{
+
+}
+
